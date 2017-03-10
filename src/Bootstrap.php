d4d276dc78a804153977f64c293e3e8aec83b2ac
Charles DELFLY <c.delfly@linkbynet.com>
2017-03-10 09:40:15 +0100

    Split boostrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace macfly\user\server;
+
+use Yii;
+
+class Bootstrap implements \yii\base\BootstrapInterface
+{
+  /** @inheritdoc */
+  public function bootstrap($app)
+  {
+		if ($app->hasModule('userapi') && ($module = $app->getModule('userapi')) instanceof Module)
+		{
+			if ($app instanceof ConsoleApplication)
+			{
+				$module->controllerNamespace = 'macfly\user\server\commands';
+			} else
+			{
+				$configUrlRule = [
+					'class'		=> 'yii\web\GroupUrlRule',
+					'prefix'	=> $module->urlPrefix,
+					'rules'		=> $module->urlRules,
+				];
+
+				if ($module->urlPrefix != 'userapi')
+				{
+					$configUrlRule['routePrefix'] = 'userapi';
+				}
+
+				$rule	= Yii::createObject($configUrlRule);
+
+				$app->urlManager->addRules([$rule], false);
+			}
+		}
+  }
+}
